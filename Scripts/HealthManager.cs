fd60a34d109960b2291408044ac0276c8be95b64
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2021-01-14 12:35:23 +0200

    Added scripts and prefabs directories.
    Added NPC components to prefabs.
    Scripts directory contains: HealthManager.cs
                                              SignalListener.cs
                                              Interactable.cs
                                              npc/BoundedNPC.cs
                                              ScriptableObjects/FloatValue.cs
                                              ScriptableObjects/IntValue.cs
                                              ScriptableObjects/Signal.cs

---
+++ Scripts/HealthManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image healthFill;
+    public FloatValue playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.maxValue = playerHealth.initialValue;
+    }
+
+    public void UpdateHealth()
+    {
+        slider.value = playerHealth.initialValue;
+        healthFill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ Scripts/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public bool playerInRange;
+    public Signal context;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if other is "Player". collision box
+        if (other.CompareTag("player"))
+        {
+            //context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // Check if other is "Player". collision box
+        if (other.CompareTag("player") && !other.isTrigger)
+        {
+            //context.Raise();
+            playerInRange = false;
+        }
+    }
+}
